ea12a9eb97295d9bd0430ed33edae0a251911b88
Devskiller <owl@devskiller.com>
2022-09-30 17:33:06 +0000

    Candidate repo initialization

---
+++ src/test/java/com/devskiller/tasks/blog/service/PostServiceTest.java
@@ -0,0 +1,48 @@
+package com.devskiller.tasks.blog.service;
+
+import com.devskiller.tasks.blog.model.Post;
+import com.devskiller.tasks.blog.model.dto.PostDto;
+import com.devskiller.tasks.blog.repository.PostRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.springframework.test.util.AssertionErrors.assertNotNull;
+
+@SpringBootTest
+public class PostServiceTest {
+
+	@Autowired
+	PostRepository postRepository;
+
+	@Autowired
+	PostService postService;
+
+	@Test
+	public void shouldReturnCreatedPost() {
+		Post post = new Post();
+		post.setTitle("Test title");
+		post.setContent("Test content");
+		LocalDateTime creationDate = LocalDateTime.of(2018, 5, 20, 20, 51, 16);
+		post.setCreationDate(creationDate);
+		postRepository.save(post);
+
+		PostDto postDto = postService.getPost(post.getId());
+
+		assertNotNull("Post shouldn't be null", postDto);
+		assertEquals("Test content", postDto.getContent());
+		assertEquals("Test title", postDto.getTitle());
+		assertEquals(creationDate, postDto.getCreationDate());
+	}
+
+	@Test
+	public void shouldReturnNullForNotExistingPost() {
+		PostDto postDto = postService.getPost("123");
+
+		assertNull(postDto);
+	}
+}
