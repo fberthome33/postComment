dd6b5d9e57fc491949bff5095558a19fed73e4f4
Fabien Berthomé <fabien.berthome.ext@veolia.com>
2022-10-01 15:28:45 +0200

    Add Post Test

--- src/main/java/com/devskiller/tasks/blog/BlogApplication.java
+++ src/main/java/com/devskiller/tasks/blog/BlogApplication.java
@@ -2,8 +2,10 @@ package com.devskiller.tasks.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = {"com.devskiller.tasks.blog"})
 public class BlogApplication {
 
 	public static void main(String[] args) {
