ab3916576e024fd22953073cd1c0e0759234dc7c
Fabien Berthomé <fabien.berthome.ext@veolia.com>
2022-10-01 15:44:41 +0200

    Rollback configuration

--- src/main/java/com/devskiller/tasks/blog/BlogApplication.java
+++ src/main/java/com/devskiller/tasks/blog/BlogApplication.java
@@ -2,10 +2,13 @@ package com.devskiller.tasks.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackages = {"com.devskiller.tasks.blog"})
 public class BlogApplication {
 
 	public static void main(String[] args) {
