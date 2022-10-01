5af185355a8751ed0195fdf89281ceba4addc7ad
Fabien Berthomé <fabien.berthome.ext@veolia.com>
2022-10-01 14:33:49 +0200

    Solution

---
+++ src/main/java/com/devskiller/tasks/blog/rest/CommentController.java
@@ -0,0 +1,38 @@
+package com.devskiller.tasks.blog.rest;
+
+import com.devskiller.tasks.blog.model.dto.CommentDto;
+import com.devskiller.tasks.blog.model.dto.NewCommentDto;
+import com.devskiller.tasks.blog.model.dto.PostDto;
+import com.devskiller.tasks.blog.service.CommentService;
+import com.devskiller.tasks.blog.service.PostService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RestController
+@RequestMapping("/posts")
+public class CommentController {
+
+	private final CommentService commentService;
+
+	public CommentController(CommentService commentService) {
+		this.commentService = commentService;
+	}
+
+	@GetMapping(value = "/{postId}/comments")
+	@ResponseStatus(HttpStatus.OK)
+	public List<CommentDto> getComments(@PathVariable String postId) {
+		return commentService.getCommentsForPost(postId);
+	}
+
+	@PostMapping(value = "/{id}/comments")
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<String> addPost(@RequestBody NewCommentDto commentDto) {
+		String commentId= commentService.addComment(commentDto);
+		return ResponseEntity.status(201).body(commentId);
+	}
+}
